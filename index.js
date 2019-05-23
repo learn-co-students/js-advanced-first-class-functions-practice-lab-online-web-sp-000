3d3a045834f84f6d91a1d690f719641f7c8842b0
jonmackeyparty <jon@jonmackey.party>
2019-05-23 17:06:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+function logDriverNames(array) {
+  const logger = function(el, i, arr) {
+    console.log(el.name);
+  };
+  array.forEach(logger);
+}
+
+function logDriversByHometown(array, hometown) {
+  const logger = function(el, i, arr) {
+    if (el.hometown === hometown) {
+          console.log(el.name);
+        }
+    };
+  array.forEach(logger);
+}
+
+function driversByRevenue(array) {
+  const sorter = function (a, b) {
+    return a.revenue - b.revenue;
+  };
+  let newArray = array.slice(0).sort(sorter);
+  return newArray;
+}
+
+function driversByName(array) {
+  const sorter = function (a, b) {
+    return a.name.localeCompare(b.name)
+  };
+  let newArray = array.slice(0).sort(sorter);
+  return newArray;
+}
+
+function totalRevenue(array, value) {
+  const revreducer = function(agg, el, i, arr) {
+    return agg + el.revenue;
+  }
+  return array.reduce(revreducer, 0)
+}
+
+function averageRevenue(array, value) {
+  const revreducer = function(agg, el, i, arr) {
+    return (agg + el.revenue);
+  }
+  return (array.reduce(revreducer, 0))/array.length;
+}
