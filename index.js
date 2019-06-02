55817f261e12d545c7427ccb17244384ea17ad72
Btate712 <btate712@gmail.com>
2019-06-02 15:01:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 // Code your solution in this file!
-function logDriverNames(drivers) {
-  return drivers.forEach(function (driver) {
+const logDriverNames = function (drivers) {
+  drivers.forEach(function (driver) {
     console.log(driver.name);
   });
 }
 
-function logDriversByHometown(drivers, location) {
-  return drivers.forEach(function (driver) {
+const logDriversByHometown = function (drivers, location) {
+  drivers.forEach(function (driver) {
     if (driver.hometown === location) {
       console.log(driver.name);
     }
   });
 }
 
-function driversByRevenue(drivers) {
-  const newDrivers = drivers.slice(0, drivers.length);
-  return newDrivers.sort (function(a, b) {
+const driversByRevenue = function (drivers) {
+  return drivers.slice().sort (function(a, b) {
     return a.revenue - b.revenue;
   });
 }
 
 function driversByName(drivers) {
-  const newDrivers = drivers.slice(0, drivers.length);
-  return newDrivers.sort (function(a, b) {
+  return drivers.slice().sort (function(a, b) {
     return a.name.localeCompare(b.name);
   });
 }
 
 function totalRevenue(drivers) {
-  return drivers.reduce(function (total, driver, i, arr) {
+  return drivers.reduce(function (total, driver) {
     return total + driver.revenue;
   }, 0);
 }
