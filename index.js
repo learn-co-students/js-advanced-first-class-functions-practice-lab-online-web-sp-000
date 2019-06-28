a19073dccbaff1bdc22c3560e9bc59596b568597
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-28 00:00:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function logDriverNames(driverArr) {
+  driverArr.forEach(function(driver){
+    console.log(driver.name);
+  });
+};
+
+function logDriversByHometown(driverArr, location) {
+  driverArr.forEach(function(driver) {
+    if (driver.hometown === location) {
+      console.log(driver.name);
+    }
+  });
+};
+
+function driversByRevenue(driverArr) {
+  return driverArr.sort(function(a, b){
+    return a.revenue - b.revenue;
+  });
+};
+
+function driversByName(driverArr) {
+  return driverArr.sort(function(a, b){
+    return a.localeCompare(b);
+  });
+};
+
+// totalRevenue() — Receives an array of driver objects and returns the sum of the revenue earned by each driver.
+// averageRevenue() — Receives an array of driver objects and returns the average revenue earned by each driver.
