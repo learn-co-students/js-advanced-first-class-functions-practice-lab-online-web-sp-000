e849ee8d8f606ddb74213565b75d403e1232fa6a
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-28 16:53:37 +0000

    Done.

--- index.js
+++ index.js
@@ -14,16 +14,24 @@ function logDriversByHometown(driverArr, location) {
 };
 
 function driversByRevenue(driverArr) {
-  return driverArr.sort(function(a, b){
+  return driverArr.slice().sort(function(a, b){
     return a.revenue - b.revenue;
   });
 };
 
 function driversByName(driverArr) {
-  return driverArr.sort(function(a, b){
-    return a.localeCompare(b);
+  return driverArr.slice().sort(function(a, b){
+    return a.name.localeCompare(b.name);
   });
 };
 
-// totalRevenue() — Receives an array of driver objects and returns the sum of the revenue earned by each driver.
-// averageRevenue() — Receives an array of driver objects and returns the average revenue earned by each driver.
+
+function totalRevenue(driverArr) {
+  return driverArr.reduce(function(sum, d) {
+    return sum += d.revenue;
+  }, 0);
+};
+
+function averageRevenue (driverArr) {
+  return driverArr.reduce(function(sum, d){return sum += d.revenue;},0) / driverArr.length;
+};
